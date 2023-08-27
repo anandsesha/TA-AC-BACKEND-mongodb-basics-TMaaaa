66a28ed6bba437d49811f5db15344187a1131365
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2023-08-26 23:21:22 -0400

    Assignment lvl II

---
+++ block-BMaaah/data.js
@@ -0,0 +1,26 @@
+let articlesArray = [
+  {
+    _id: '003',
+    title: 'Article-3',
+    date: 2021,
+    details: 'Reptiles',
+    author: { name: 'kb', email: 'kb@gmail.com', age: 30 },
+    tags: ['React', 'Express'],
+  },
+  {
+    _id: '002',
+    title: 'Article-2',
+    date: 2022,
+    details: 'Mamals',
+    author: { name: 'Arvind', email: 'arv@gmail.com', age: 24 },
+    tags: ['HTML', 'css'],
+  },
+  {
+    _id: '001',
+    title: 'Article-1',
+    date: 2023,
+    details: 'Birds',
+    author: { name: 'Anand', email: 'anand@gmail.com', age: 27 },
+    tags: ['NodeJS', 'mongoDB'],
+  },
+];
